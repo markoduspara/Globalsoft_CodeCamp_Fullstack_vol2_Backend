3515ad0838becc39f4f1248059f92c38f1985c44
Jure <jurekvesic98@gmail.com>
2024-04-22 19:17:08 +0200

    ispravka sql

--- game_state.sql
+++ game_state.sql
@@ -1,9 +1,9 @@
-CREATE TABLE  `game_state` {
+CREATE TABLE  `game_state` (
     `id`          INT     UNSIGNED    NOT NULL AUTO_INCREMENT,
     `name`        INT                 NOT NULL,
     `created_at` TIMESTAMP            DEFAULT CURRENT_TIMESTAMP NOT NULL,
     `updated_at` TIMESTAMP            DEFAULT CURRENT_TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (id),
     UNIQUE KEY gs_name (name)
-};
+);
 
