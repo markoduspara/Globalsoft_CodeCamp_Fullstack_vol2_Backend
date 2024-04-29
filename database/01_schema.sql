a119f6cac7635e82d8eb9e19bb5fb3d82f9246b9
Marko Duspara <m.duspara@globalsoft.ba>
2024-04-29 18:20:30 +0200

    db struct

--- database/01_schema.sql
+++ database/01_schema.sql
@@ -1,3 +1,14 @@
+CREATE TABLE IF NOT EXISTS user (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username varchar(64) NOT NULL,
+    'password' varchar(255) NOT NULL,
+    created_at TIMESTAMP    DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP    DEFAULT CURRENT_TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP    NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY uk_name (username,'password')
+);
+
 CREATE TABLE user_login (
     id int unsigned NOT NULL AUTO_INCREMENT,
     user_id  int unsigned NOT NULL,
@@ -6,4 +17,37 @@ CREATE TABLE user_login (
     PRIMARY KEY pk_id (id),
     UNIQUE KEY uk_user_id (user_id),
     FOREIGN KEY fk_user_id (user_id) REFERENCES user(id)
+);
+
+
+CREATE TABLE  `game_state` (
+    `id`          INT     UNSIGNED    NOT NULL AUTO_INCREMENT,
+    `name`        INT                 NOT NULL,
+    `created_at` TIMESTAMP            DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP            DEFAULT CURRENT_TIMESTAMP NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    UNIQUE KEY gs_name (name)
+);
+
+CREATE TABLE game (
+    "id" INT AUTO_INCREMENT PRIMARY KEY,
+    "player1Id" INT UNSIGNED NOT NULL,
+    "player2Id" INT UNSIGNED NOT NULL,
+    "player1State" int UNSIGNED NULL,
+    "player2State" int UNSIGNED NULL,
+    "n_game" tinyint(1) NOT NULL
+
+    FOREIGN KEY("player1_id") REFERENCES "user"("id")
+    FOREIGN KEY("player2_id") REFERENCES "user"("id")
+    FOREIGN KEY("player1State") REFERENCES "game_state"("id")
+    FOREIGN KEY("player2State") REFERENCES "game_state"("id")
+);
+
+create table gameGrid (
+    id tinyint primary key AUTO_INCREMENT NOT NULL ,
+    game_id tinyint NOT NULL ,
+    stateValue varchar,
+    n tinyint NOT NULL,
+    FOREIGN KEY (game_id) REFERENCES game(id)
+    UNIQUE KEY (game_id,n)
 );
