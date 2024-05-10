c853ccf6a7658a8fe0557dcae79bfd79f153920b
Marko Duspara <m.duspara@globalsoft.ba>
2024-05-10 18:19:39 +0200

    testing nest db conn

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'user',
+      password: 'password',
+      database: 'db',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
