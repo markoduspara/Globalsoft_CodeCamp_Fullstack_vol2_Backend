79859866d880447084c0265eba99249c74e56bce
Marko Duspara <m.duspara@globalsoft.ba>
2024-05-17 18:22:28 +0200

    add first migration

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,19 @@
+import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+config({ path: resolve(__dirname, '.env') });
+
+const DataSourceConfig = new DataSource({
+  type: 'mysql',
+  host: 'localhost', //process.env.DB_HOST,
+  port: 3306, //Number(process.env.DB_PORT),
+  username: 'user', //process.env.MEST_NEW_USER,
+  password: 'password', //process.env.MEST_NEW_PASSWORD,
+  database: 'db', //process.env.MEST_NEW_DATABASE,
+  migrations: ['./src/migrations/*'],
+  synchronize: false,
+  migrationsTableName: 'migrations',
+});
+
+export default DataSourceConfig;
--- src/game/game.service.ts
+++ src/game/game.service.ts
@@ -7,34 +7,36 @@ import { Repository } from 'typeorm';
 import { ResponseGameDto } from './dto/response-game.dto';
 
 @Injectable()
-  export class GameService {
-    constructor(
-      @InjectRepository(Game)
-      private gameRepository: Repository<Game>,
-    ) { }
-    async create(createGameDto: CreateGameDto): Promise<ResponseGameDto> {
-      try {
-        const gameCreate = await this.gameRepository.save(createGameDto);
-        return { id: gameCreate.id }
-      } catch(error) { 
-        throw new InternalServerErrorException(`${error.message}`);
-      }
+export class GameService {
+  constructor(
+    @InjectRepository(Game)
+    private gameRepository: Repository<Game>,
+  ) {}
+  async create(createGameDto: CreateGameDto): Promise<ResponseGameDto> {
+    try {
+      const gameCreate = await this.gameRepository.save(createGameDto);
+      return { id: gameCreate.id };
+    } catch (error) {
+      throw new InternalServerErrorException(`${error.message}`);
     }
-    
+  }
+
   findAll() {
     return this.gameRepository.find({});
   }
 
   findOne(id: number) {
-    return this.gameRepository.findOne({ where: { id}});
+    return this.gameRepository.findOne({ where: { id } });
   }
 
-  async update(id: number, updateGameDto: UpdateGameDto): Promise<ResponseGameDto> {
-    try
-    {
+  async update(
+    id: number,
+    updateGameDto: UpdateGameDto,
+  ): Promise<ResponseGameDto> {
+    try {
       const gameUpdate = await this.gameRepository.update(id, updateGameDto);
-      return this.gameRepository.findOne( { where: { id}});
-    } catch(error) { 
+      return this.gameRepository.findOne({ where: { id } });
+    } catch (error) {
       throw new InternalServerErrorException(`${error.message}`);
     }
   }
